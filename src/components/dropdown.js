e754aa9de2abaa68492234791fa1d9f3148d3920
Abhinav <cabhinav.0001@gmail.com>
2023-02-11 15:33:03 +0530

    new change

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,33 +1,48 @@
 import { useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
 
-const DropDown = ({options, value, onChange}) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-    };
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const optionSelect = (option) => {
-        setIsOpen(false);
-        console.log(option);
-        onChange(option);
-    };
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const renderedOptions = options.map((option) => {
-        return (
-            <div key={option.value} className="hover:bk-sky-100 rounded p-1 cursor-pointer" onClick={() => optionSelect(option)}>
-                {option.label}
-            </div>
-        );
-    });
+  const handleOptionClick = (option) => {
+    // CLOSE DROPDOWN
+    setIsOpen(false);
+    // WHAT OPTION DID THE USER CLICK ON???
+    onChange(option);
+  };
 
-    // const content = selection ? selection : "Select an option";
+  const renderedOptions = options.map((option) => {
     return (
-        <div className='w-48 relvative'>
-            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>{value?.label || 'Select..'}</div>
-             {   isOpen && <div className='absolute top-full border rounded p-3'>{renderedOptions}</div>}
-        </div>
-                            
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
     );
-};
+  });
 
-export default DropDown;
+  return (
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
